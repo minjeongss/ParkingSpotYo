790313950251306b962f63971b7d7a59f1ed5588
minjeongss <dotoriido@gmail.com>
2024-09-20 01:47:46 +0900

    style: 환경 설정 주석 추가 및 README 수정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,11 +2,13 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    //설정
     // 'eslint:recommended',
+    // 'plugin:react/recommended',
     'airbnb',
     'airbnb-typescript',
     'airbnb/hooks',
-    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:react-hooks/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -14,17 +16,19 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    //설정
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: ['./tsconfig.json', './tsconfig.app.json', './tsconfig.node.json'],
     tsconfigRootDir: __dirname,
   },
-  plugins: ['react-refresh', 'react', '@typescript-eslint', 'prettier'],
+  plugins: ['react-refresh', 'react', '@typescript-eslint', 'prettier'], //설정
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    'react/react-in-jsx-scope': 'off',
+    //설정
+    // 'react-refresh/only-export-components': [
+    //   'warn',
+    //   { allowConstantExport: true },
+    // ],
+    'react/react-in-jsx-scope': 'off', //react17 이상을 위한 설정
   },
 }
