4ecc10720e6adcb4cd42ba46e014aa14c2612a6e
minjeongss <dotoriido@gmail.com>
2024-09-22 15:54:31 +0900

    feat: 지도 마커 표시 구현

--- src/components/features/map/Map.tsx
+++ src/components/features/map/Map.tsx
@@ -1,29 +1,100 @@
 import styled from '@emotion/styled'
 import { useEffect } from 'react'
+import {
+  MarkerData,
+  MapInstance,
+  MarkerInstance,
+  InfoInstance,
+} from '../../../types/kakao'
 
 const MapContainer = styled.div({
   width: '100%', // 너비 설정
   height: '400px', // 높이 설정
-  color: 'orange',
 })
 
 const Map = () => {
   useEffect(() => {
-    const container = document.getElementById('map')
-    if (!container) {
-      return
-    }
-
     /**
      * center: 지도의 중심 좌표
      * level: 지도의 확대 레벨
+     * position: 마커의 좌표
      */
     const options = {
       center: new window.kakao.maps.LatLng(33.450701, 126.570667),
       level: 3,
     }
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const markers: MarkerData[] = [
+      {
+        title: '카카오',
+        content: '<div>카카오</div>',
+        latlng: new window.kakao.maps.LatLng(33.450705, 126.570677),
+      },
+      {
+        title: '생태연못',
+        content: '<div>카카오</div>',
+        latlng: new window.kakao.maps.LatLng(33.450936, 126.569477),
+      },
+      {
+        title: '텃밭',
+        content: '<div>카카오</div>',
+        latlng: new window.kakao.maps.LatLng(33.450879, 126.56994),
+      },
+      {
+        title: '근린공원',
+        content: '<div>카카오</div>',
+        latlng: new window.kakao.maps.LatLng(33.451393, 126.570738),
+      },
+    ]
+
+    const container = document.getElementById('map')
+    if (!container) {
+      return
+    }
     const map = new window.kakao.maps.Map(container, options)
+    const mainMarkerPosition = new window.kakao.maps.LatLng(
+      33.450701,
+      126.570667
+    )
+    const mainMarker = new window.kakao.maps.Marker({
+      position: mainMarkerPosition,
+    })
+    mainMarker.setMap(map)
+    const makeOverListener = (
+      targetMap: MapInstance,
+      marker: MarkerInstance,
+      infowindow: InfoInstance
+    ) => {
+      return () => {
+        infowindow.open(targetMap, marker)
+      }
+    }
+    const makeOutListener = (infowindow: InfoInstance) => {
+      return () => {
+        infowindow.close()
+      }
+    }
+    markers.forEach(item => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const marker = new window.kakao.maps.Marker({
+        map,
+        position: item.latlng,
+        title: item.title,
+      })
+
+      const infowindow = new window.kakao.maps.InfoWindow({
+        content: item.content,
+      })
+      window.kakao.maps.event.addListener(
+        marker,
+        'mouseover',
+        makeOverListener(map, marker, infowindow)
+      )
+      window.kakao.maps.event.addListener(
+        marker,
+        'mouseout',
+        makeOutListener(infowindow)
+      )
+    })
   }, [])
 
   return <MapContainer id="map" />
--- src/types/kakao.d.ts
+++ src/types/kakao.d.ts
@@ -5,11 +5,25 @@ declare global {
       maps: {
         LatLng: new (lat: number, lng: number) => LatLngInstance
         Map: new (container: HTMLElement, options: MapOptions) => MapInstance
+        Marker: new (options: MarkerOptions) => MarkerInstance
+        InfoWindow: new (content) => InfoInstance
+        event: {
+          addListener: (
+            target: MarkerInstance,
+            event: string,
+            callback: () => void
+          ) => void
+        }
       }
     }
   }
 }
 
+export interface MarkerData {
+  title: string
+  content: string
+  latlng: LatLngInstance
+}
 // 카카오 지도 관련 타입 정의
 interface LatLngInstance {
   getLat: () => number
@@ -20,11 +34,21 @@ interface MapOptions {
   center: LatLngInstance
   level: number
 }
-
-interface MapInstance {
+export interface MapInstance {
   setCenter: (latlng: LatLngInstance) => void
   setLevel: (level: number) => void
-  // 추가로 필요한 메서드 정의
+}
+interface MarkerOptions {
+  map?: MapInstance | null
+  position: LatLngInstance
+  title?: string
+}
+export interface MarkerInstance {
+  setMap: (map: MapInstance) => void
+}
+export interface InfoInstance {
+  open: (map: MapInstance, marker: MarkerInstance) => void
+  close: () => void
 }
 
 export {}
