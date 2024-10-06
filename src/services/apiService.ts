135c695ca8b847f288660f9d4f5488eafb0c7f5c
minjeongss <dotoriido@gmail.com>
2024-10-06 14:57:14 +0900

    🚀 Deploy: apiService에 http//에서 //로 변경 (#16)

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -6,7 +6,7 @@ export const fetchParkingData = async (
 ): Promise<ParkingInfo[] | undefined> => {
   try {
     const response = await axios.get(
-      `http://openapi.seoul.go.kr:8088/${import.meta.env.VITE_OPEN_API_KEY}/json/GetParkingInfo/1/1000/${region}`
+      `//openapi.seoul.go.kr:8088/${import.meta.env.VITE_OPEN_API_KEY}/json/GetParkingInfo/1/1000/${region}`
     )
     const data = response.data as { GetParkingInfo: { row: ParkingInfo[] } }
     return data.GetParkingInfo.row
