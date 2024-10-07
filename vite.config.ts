6fbb5e43394209e0c61a692248069686a60029b6
minjeongss <dotoriido@gmail.com>
2024-10-07 09:37:37 +0900

    🚀 Deploy: http 사용을 위해 vercel.json, vite.config.ts에서 proxy, api path 설정 추가 (#16)

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,16 @@ import svgr from 'vite-plugin-svgr'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  server: { port: 3000 },
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://openapi.seoul.go.kr:8088',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   define: {
     __APP_ENV: process.env.VITE_VERCEL_ENV,
   },
